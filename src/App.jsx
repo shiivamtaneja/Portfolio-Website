e6e9319bf8d1652bca213072213f8569a4155fc2
ShivamTaneja <shiivamtaneja@gmail.com>
2023-04-26 19:17:55 +0530

    add header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Header from "./components/Header"
+
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">
-        Hello world!
-      </h1>
+      <Header />
+      
     </div>
   )
 }
