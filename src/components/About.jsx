6e0d86143ca41865ec49ef7e78c28370f2bd94e6
ShivamTaneja <shiivamtaneja@gmail.com>
2023-04-27 00:17:54 +0530

    fix: made about me section responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
       transition={{
         duration: 1.5
       }}
-      className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+      className='flex flex-col relative h-screen text-center md:text-left md:flex-col lg:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
       <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>About Me</h3>
 
       <motion.img
@@ -33,11 +33,11 @@ const About = () => {
           duration: 1.2,
         }}
         src='https://avatars.githubusercontent.com/u/79853285?v=4'
-        className='-mb-20 md:mb-0 mt-[520px] md:mt-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-96 md:h-80 xl:w=[500px] xl:h-[600px]'
+        className='-mb-20 md:mb-0 mt-[450px] md:mt-40 lg:mt-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-96 md:h-80 xl:w=[500px] xl:h-[600px]'
       />
       <div className='space-y-10 px-0 md:px-10 mt-20 md:mt-0'>
         <h4 className='text-3xl font-semibold underline decoration-[#FFAA00]'>I love Design, Technology and Experimentation</h4>
-        <p className='text-base'>
+        <p className='text-left'>
           I'm a 20-year-old B-Tech College student from India who loves experimenting with cutting-edge technologies. 
           I'm passionate about innovative technology, creativity, software engineering and UI/UX. Along with this, I am also fascinated by gaming, content creation, and video editing. I thrive on taking on new challenges and enjoy playing with codes to expand my knowledge. 
           In just a year, I have developed a solid foundation in web development, particularly in HTML, CSS, and JS. I actively pursue my interests by freelancing in Graphic Design and Video Editing, and I have over three years of experience working with major creators.
