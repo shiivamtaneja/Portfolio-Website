f850cfc86388e46edcbe02d5d50e8df2e29acc90
ShivamTaneja <shiivamtaneja@gmail.com>
2023-05-14 01:40:07 +0530

    feat: add experience

--- src/pages/Experience/index.jsx
+++ src/pages/Experience/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { motion } from 'framer-motion'
 import BackgroundCircles from '../../components/BackgroundCircles'
+import Timeline from './Timeline'
 
 const WorkExperience = () => {
   return (
@@ -16,10 +17,10 @@ const WorkExperience = () => {
         transition={{
           duration: 1.5
         }}
-        className='flex flex-col lg:h-screen relative text-center md:text-left lg:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center overflow-hidden'
+        className='flex flex-col h-screen relative text-center md:text-left lg:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center overflow-hidden'
       >
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl '>Experience</h3>
-        <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px] '>This Section is under Development1</h2>
+        <Timeline />
         <div className='absolute top-64'>
           <BackgroundCircles />
         </div>
