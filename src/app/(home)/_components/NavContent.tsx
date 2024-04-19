9f1450560c4a18f42a9bdd436c418889b95d0de1
ShivamTaneja <shiivamtaneja@gmail.com>
2024-04-19 11:50:42 +0530

    update navbar
    Signed-off-by: ShivamTaneja <shiivamtaneja@gmail.com>

---
+++ src/app/(home)/_components/NavContent.tsx
@@ -0,0 +1,116 @@
+import { DrawerClose } from '@/components/ui/drawer';
+import { navLinks, socialIcons } from '@/lib/constants';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useEffect, useRef, useState } from 'react';
+import { SocialIcon } from 'react-social-icons';
+
+const NavContent = ({
+  navOpen
+}: {
+  navOpen: boolean
+}) => {
+  const navBarRef = useRef(null);
+
+  useGSAP(() => {
+    const t1 = gsap.timeline({ paused: true })
+
+    if (navOpen) {
+      t1.play();
+    }
+
+    t1.fromTo('.nav-item', {
+      y: 50,
+      opacity: 0
+    }, {
+      y: 0,
+      opacity: 1,
+      duration: 0.5,
+      stagger: 0.1,
+    })
+
+  }, { scope: navBarRef });
+
+  return (
+    <>
+      <div
+        className="w-screen h-full bg-accent-300 rounded-md pointer-events-auto relative max-w-xl flex flex-col"
+      >
+        <div className='flex justify-between px-5 py-2'>
+          <div></div>
+          <DrawerClose asChild>
+            <button
+              className='rounded-full flex h-16 w-16 flex-col items-center justify-center bg-accent-400 relative transition-all duration-100 ease-in-out hover:scale-90 scale-100 z-[20]'
+            >
+              <span className="absolute h-[2px] w-7 rounded-full bg-dark 2xl:w-9 rotate-45" />
+              <span className="absolute h-[2px] w-7 rounded-full bg-dark 2xl:w-9 -rotate-45" />
+            </button>
+          </DrawerClose>
+        </div>
+        <div className='h-full '>
+          <div className="absolute left-0 top-0 opacity-40">
+            <Image
+              src={'/navmenu.svg'}
+              alt=''
+              loading='lazy'
+              width={245}
+              height={245}
+              className=''
+            >
+            </Image>
+          </div>
+          <div
+            className='flex flex-col justify-around h-full overflow-hidden'
+            ref={navBarRef}
+          >
+            <nav
+              className='text-dark relative flex flex-col justify-center px-8 font-bold sm:px-14 text-heading-3 2xl:px-20 overflow-hidden'
+            >
+              {navLinks.map((data, index) => (
+                <div className='overflow-hidden' key={index}>
+                  <div
+                    className="group flex w-fit cursor-pointer items-center gap-x-4 nav-item"
+                  >
+                    <span className="invisible inline-block h-3 w-3 scale-0 rounded-full bg-darkest opacity-0 transition-all duration-800 group-hover:visible group-hover:scale-100 group-hover:opacity-100">
+                    </span>
+                    <div className="w-fit overflow-y-clip">
+                      <DrawerClose asChild>
+                        <a
+                          href={data.path}
+                          // scroll={false}
+                          className='inline-block transition-transform duration-700 group-hover:translate-x-6'
+                        >
+                          {data.name}
+                        </a>
+                      </DrawerClose>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </nav>
+            <div className='flex sm:justify-end justify-center'>
+              {socialIcons.map((socials, index) => (
+                <SocialIcon
+                  style={{
+                    width: 80,
+                    height: 80
+                  }}
+                  url={socials.link}
+                  fgColor='gray'
+                  bgColor='transparent'
+                  target='_blank'
+                  key={index}
+                  className='hover:scale-90 scale-100 duration-100 transition-all'
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default NavContent
--- src/app/(home)/_components/Navbar.tsx
+++ src/app/(home)/_components/Navbar.tsx
@@ -1,16 +1,20 @@
 "use client";
 
 import Link from 'next/link';
-import { useEffect, useRef } from "react";
+import { useRef, useState } from "react";
 
-import { delays, socialIcons } from '@/lib/constants';
+import { Drawer, DrawerContent, DrawerTrigger } from '@/components/ui/drawer';
+import { delays } from '@/lib/constants';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import React from 'react';
-import { SocialIcon } from 'react-social-icons';
+
 import Logo from "./Logo";
+import NavContent from './NavContent';
 
 const Navbar = () => {
+  const [navOpen, setNavOpen] = useState(false);
+
   const headerRef = useRef(null);
 
   useGSAP(() => {
@@ -22,31 +26,40 @@ const Navbar = () => {
       delay: delays['pre-loader'],
       duration: 0.75
     });
-  });
+  }, { scope: headerRef });
 
   return (
-    <header className='flex w-full px-5 py-3 max-w-screen-xl mx-auto justify-between' ref={headerRef}>
-      <Link
-        href=''
-        className="flex items-center justify-center gap-12"
-      >
-        <Logo />
-      </Link>
-      <div>
-        {socialIcons.map((socials, index) => (
-          <SocialIcon
-            style={{
-              width: 80,
-              height: 80
-            }}
-            url={socials.link}
-            fgColor='gray'
-            bgColor='transparent'
-            target='_blank'
-            key={index}
-          />
-        ))}
-      </div>
+    <header className='fixed top-0 z-[15] w-full' ref={headerRef}>
+      <nav className='flex justify-between px-4 items-center py-3 max-w-screen-2xl mx-auto'>
+        <Link
+          href={'/'}
+          className=''
+          aria-label='Back to home'
+        >
+          <Logo />
+        </Link>
+        <Link
+          href={'/#about'}
+          scroll={false}
+          className='inline-block transition-transform duration-700 group-hover:translate-x-6'
+        >
+          click
+        </Link>
+        <Drawer direction='right'>
+          <DrawerTrigger
+            className='rounded-full flex h-16 w-16 flex-col items-center justify-center bg-accent-300 relative transition-all duration-100 ease-in-out hover:scale-90 scale-100'
+            onClick={() => setNavOpen(true)}
+          >
+            <span className="absolute h-[2px] w-7 rounded-full bg-dark 2xl:w-9 -translate-y-1" />
+            <span className="absolute h-[2px] w-7 rounded-full bg-dark 2xl:w-9 translate-y-1" />
+          </DrawerTrigger>
+          <DrawerContent>
+            <NavContent
+              navOpen={navOpen}
+            />
+          </DrawerContent>
+        </Drawer>
+      </nav>
     </header>
   );
 };
