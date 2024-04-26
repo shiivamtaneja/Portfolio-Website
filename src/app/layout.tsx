2e523e7d663f55f4d74cca92217a29483284445b
ShivamTaneja <shiivamtaneja@gmail.com>
2024-04-27 00:47:06 +0530

    update: remove Page not found title from common layout.tsx
    Signed-off-by: ShivamTaneja <shiivamtaneja@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import "./app.css";
 const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Page Not Found | Shivam Taneja",
+  title: "Shivam Taneja",
   description: "Personal portfolio website for Shivam Taneja",
   icons: {
     icon: [
