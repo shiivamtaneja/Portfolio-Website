e068f60631d59b88ede759ced3240dbe03f9723e
Shivam Taneja <shiivamtaneja@gmail.com>
2025-08-28 20:02:09 +0530

    add: gtm support to website
    Signed-off-by: Shivam Taneja <shiivamtaneja@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
+import Script from "next/script";
 
 import "./globals.css";
 
@@ -40,9 +41,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script id="gtm-init" strategy="afterInteractive">
+          {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WVPS3JFL');
+      `}
+        </Script>
+      </head>
       <body
         className={`${spaceGrotesk.className} antialiased bg-zinc-900`}
       >
+        {/* Google Tag Manager (noscript */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WVPS3JFL"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <GrainyFilter />
         <ToastContainer theme="dark" />
         <SmoothScroll>
