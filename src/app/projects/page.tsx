fbc90f3bc77cc64d4ced37358b61af20f382cb6f
Shivam Taneja <shiivamtaneja@gmail.com>
2025-09-29 15:15:34 +0530

    fix: build
    Signed-off-by: Shivam Taneja <shiivamtaneja@gmail.com>

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -34,7 +34,7 @@ const ProjectsPage = () => {
         <div className="flex flex-col gap-2">
           <h1 className='text-3xl font-bold'>All Projects</h1>
           <p className='text-neutral-400'>
-            A collection of projects I've built, ranging from AI-powered tools to collaborative platforms.
+            A collection of projects I&apos;ve built, ranging from AI-powered tools to collaborative platforms.
           </p>
         </div>
 
