01f98acb305ac0f80dcf38cd2adba108b978d5ba
ShivamTaneja <shiivamtaneja@gmail.com>
2026-03-06 12:07:08 +0530

    fix: theme support for chatbot details page

--- src/app/projects/(projects)/chat-bot/highlight-chatbot.tsx
+++ src/app/projects/(projects)/chat-bot/highlight-chatbot.tsx
@@ -9,16 +9,16 @@ function HighlightChatbot() {
 
   return (
     <ul className="flex list-disc ml-4 flex-col gap-2">
-      <li className='text-neutral-400 relative w-fit' onClick={() => highlight()}>
-        <span className="text-white cursor-pointer hover-animation">View Live Project</span>
+      <li className='relative w-fit' onClick={() => highlight()}>
+        <span className="cursor-pointer hover-animation">View Live Project</span>
       </li>
 
-      <li className='text-neutral-400 relative w-fit' onClick={() => highlight()}>
+      <li className='relative w-fit' onClick={() => highlight()}>
         <Link
           href="https://github.com/shiivamtaneja/Portfolio-Website/tree/design-v2"
           rel="noopener noreferrer"
           target="_blank"
-          className="text-white relative overflow-hidden"
+          className="relative overflow-hidden"
         >
           <span className="hover-animation">View source code</span>
         </Link>
